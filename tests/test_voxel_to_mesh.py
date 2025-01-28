d96ebe1c9dafffb6babb42b6379c9d6e01ac4eaf
Andrew T Polonsky <apolon@sandia.gov>
2025-01-28 13:54:29 -0800

    relative path on test

--- tests/test_voxel_to_mesh.py
+++ tests/test_voxel_to_mesh.py
@@ -9,13 +9,13 @@ import pytest
 
 # local libraries
 from recon3d import voxel_to_mesh as vm
-from recon3d.static_test_paths import *
+from recon3d import static_test_paths as test_paths
 
 
 def test_letter_f_3d():
     """Tests that the letter_f_3d.npy example can be run."""
-    path_yml = LETTER_F_3D_YML
-    path_exo = LETTER_F_3D_EXO
+    path_yml = test_paths.LETTER_F_3D_YML
+    path_exo = test_paths.LETTER_F_3D_EXO
 
     result = vm.voxel_to_mesh(yml_input_file=path_yml)
 
